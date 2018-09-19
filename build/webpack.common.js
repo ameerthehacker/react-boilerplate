fd6cf7e3814550fedc011323198577c8c428ada7
ameerthehacker <ameerjhanprof@gmail.com>
2018-09-19 20:17:19 +0530

    Add scss support

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const isDevEnv = process.env.NODE_ENV === 'development';
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const config = {
   entry: path.resolve(process.cwd(), 'src', 'App.tsx'),
@@ -7,12 +8,26 @@ const config = {
     filename: isDevEnv? 'app.bunble.js': 'app.[hash].bundle.js',
     path: path.resolve(process.cwd(), 'dist')
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: isDevEnv? '[name].bundle.css': '[name].[hash].bundle.css',
+      chunkFilename: isDevEnv? '[id].chunk.css': '[id].[hash].chunk.css'
+    })
+  ],
   module: {
     rules: [
       {
         test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          isDevEnv? 'style-loader': MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
       }
     ]
   }
