65d659f0a4503430c7ed6a8ab7fd68e50c275f1b
ameerthehacker <ameerjhanprof@gmail.com>
2018-09-19 20:24:51 +0530

    Move webpack.config.js also into build folder

---
+++ build/webpack.config.js
@@ -0,0 +1,8 @@
+const isDevEnv = process.env.NODE_ENV === 'development';
+
+if(isDevEnv) {
+  module.exports = require('./webpack.dev');
+}
+else {
+  module.exports = require('./webpack.prod');
+}
--- webpack.config.js
+++
@@ -1,8 +0,0 @@
-const isDevEnv = process.env.NODE_ENV === 'development';
-
-if(isDevEnv) {
-  module.exports = require('./build/webpack.dev');
-}
-else {
-  module.exports = require('./build/webpack.prod');
-}
