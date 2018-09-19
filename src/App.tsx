c462bb45c9c1e4fba3a52b412660407e5ae8f2fe
ameerthehacker <ameerjhanprof@gmail.com>
2018-09-20 00:21:59 +0530

    Add tslint support

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import * as React from 'react';
-import * as ReactDom from 'react-dom';
+import * as React from "react";
+import * as ReactDom from "react-dom";
 // CSS assets
-import './scss/app.scss';
+import "./scss/app.scss";
 
 export class App extends React.Component {
   constructor(props: any) {
     super(props);
   }
 
-  render() {
+  public render() {
     return (<h1>Hello React</h1>);
   }
 }
 
-ReactDom.render(<App />, document.getElementById('react-root'));
+ReactDom.render(<App />, document.getElementById("react-root"));
